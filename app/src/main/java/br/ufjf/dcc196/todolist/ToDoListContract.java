382d228b32a067a54628776e13303d220eeaa11c
Jônatas André <jjsfandre@gmail.com>
2019-05-23 20:33:46 -0300

    Corrigindo contrato

--- app/src/main/java/br/ufjf/dcc196/todolist/ToDoListContract.java
+++ app/src/main/java/br/ufjf/dcc196/todolist/ToDoListContract.java
@@ -7,53 +7,51 @@ public final class ToDoListContract {
 
     public static class Tarefa implements BaseColumns {
 
-        //region TAREFA
-        public static final String TABLE_NAME_TAREFA="Tarefa";
-        public static final String COLLUMN_TAREFA_TITULO="titulo";
-        public static final String COLLUMN_TAREFA_DESCRICAO="descricao";
-        public static final String COLLUMN_TAREFA_DIFICULDADE="dificuldade";
-        public static final String COLLUMN_TAREFA_DTHORALIMITE="dthoralimite";
-        public static final String COLLUMN_TAREFA_DTHORAATUALIZACAO="dthoraatualizacao";
-        public static final String COLLUMN_TAREFA_STATUSID="statusid";
+        public static final String TABLE_NAME = "Tarefa";
+        public static final String COLLUMN_TITULO = "titulo";
+        public static final String COLLUMN_DESCRICAO = "descricao";
+        public static final String COLLUMN_DIFICULDADE = "dificuldade";
+        public static final String COLLUMN_DTHORALIMITE = "dthoralimite";
+        public static final String COLLUMN_DTHORAATUALIZACAO = "dthoraatualizacao";
+        public static final String COLLUMN_STATUSID = "statusid";
 
-        public static final String CREATE_TABLE_TAREFA=String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+        public static final String CREATE_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
                         "%s TEXT, %s TEXT, %s INTEGER, %s TEXT, %s TEXT, %s INTEGER)",
-                TABLE_NAME_TAREFA,_ID,COLLUMN_TAREFA_TITULO,COLLUMN_TAREFA_DESCRICAO,COLLUMN_TAREFA_DIFICULDADE,
-                COLLUMN_TAREFA_DTHORALIMITE,COLLUMN_TAREFA_DTHORAATUALIZACAO,COLLUMN_TAREFA_STATUSID
+                TABLE_NAME, _ID, COLLUMN_TITULO, COLLUMN_DESCRICAO, COLLUMN_DIFICULDADE,
+                COLLUMN_DTHORALIMITE, COLLUMN_DTHORAATUALIZACAO, COLLUMN_STATUSID
         );
-        public static final String DROP_TABLE_TAREFA=String.format("DROP TABLE %s", TABLE_NAME_TAREFA);
-        //endregion
+        public static final String DROP_TABLE = String.format("DROP TABLE %s", TABLE_NAME);
 
-        //region STATUS
-        public static final String TABLE_NAME_STATUS="Status";
-        public static final String COLLUMN_STATUS_NOME="nome";
+    }
+    public static class Status implements BaseColumns {
+        public static final String TABLE_NAME = "Status";
+        public static final String COLLUMN_NOME = "nome";
 
-        public static final String CREATE_TABLE_STATUS=String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "%s TEXT)",TABLE_NAME_STATUS,_ID,COLLUMN_STATUS_NOME
+        public static final String CREATE_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "%s TEXT)", TABLE_NAME, _ID, COLLUMN_NOME
         );
-        public static final String DROP_TABLE_STATUS=String.format("DROP TABLE %s", TABLE_NAME_STATUS);
-        //endregion
+        public static final String DROP_TABLE = String.format("DROP TABLE %s", TABLE_NAME);
+    }
 
-        //region TAG
-        public static final String TABLE_NAME_TAG="Tag";
-        public static final String COLLUMN_TAG_NOME="nome";
+    public static class Tag implements BaseColumns {
+        public static final String TABLE_NAME = "Tag";
+        public static final String COLLUMN_NOME = "nome";
 
-        public static final String CREATE_TABLE_TAG=String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "%s TEXT)",TABLE_NAME_TAG,_ID,COLLUMN_TAG_NOME
+        public static final String CREATE_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "%s TEXT)", TABLE_NAME, _ID, COLLUMN_NOME
         );
-        public static final String DROP_TABLE_TAG=String.format("DROP TABLE %s", TABLE_NAME_TAG);
-        //endregion
+        public static final String DROP_TABLE = String.format("DROP TABLE %s", TABLE_NAME);
+    }
 
-        //region TAREFATAG
-        public static final String TABLE_NAME_TAREFATAG="TarefaTag";
-        public static final String COLLUMN_TAG_STATUS="statusid";
-        public static final String COLLUMN_TAG_TAREFA="tarefaid";
+    public static class TarefaTag implements BaseColumns {
+        public static final String TABLE_NAME="TarefaTag";
+        public static final String COLLUMN_STATUS="statusid";
+        public static final String COLLUMN_TAREFA="tarefaid";
 
-        public static final String CREATE_TABLE_TAREFATAG=String.format("CREATE TABLE %s (%s INTEGER," +
-                "%s INTEGER)",TABLE_NAME_TAREFATAG,COLLUMN_TAG_STATUS,COLLUMN_TAG_TAREFA
+        public static final String CREATE_TABLE=String.format("CREATE TABLE %s (%s INTEGER," +
+                "%s INTEGER)",TABLE_NAME,COLLUMN_STATUS,COLLUMN_TAREFA
         );
-        public static final String DROP_TABLE_TAREFATAG=String.format("DROP TABLE %s", TABLE_NAME_TAREFATAG);
-        //endregion
+        public static final String DROP_TABLE=String.format("DROP TABLE %s", TABLE_NAME);
     }
 
 }
