62cb72ab5d91dad4b036a48e6cef01c7a755bac5
Jônatas André <jjsfandre@gmail.com>
2019-05-23 20:23:56 -0300

    Incluindo model Tag

---
+++ app/src/main/java/br/ufjf/dcc196/todolist/Model/Tag.java
@@ -0,0 +1,34 @@
+package br.ufjf.dcc196.todolist.Model;
+
+public class Tag {
+
+    private Long id;
+    private String nome;
+
+    public Tag(){
+
+    }
+
+    public Tag(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //region GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //endregion
+}
