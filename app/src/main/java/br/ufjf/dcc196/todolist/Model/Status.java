ebf44f26d04b4c23803c51cf6ea86b377f311e8e
Jônatas André <jjsfandre@gmail.com>
2019-05-23 20:23:50 -0300

    Incluindo model Status

---
+++ app/src/main/java/br/ufjf/dcc196/todolist/Model/Status.java
@@ -0,0 +1,33 @@
+package br.ufjf.dcc196.todolist.Model;
+
+public class Status {
+
+    private Long id;
+    private String nome;
+
+    public Status(){
+
+    }
+
+    public Status(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //region GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
