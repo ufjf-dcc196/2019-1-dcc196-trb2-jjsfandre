bd6f28749e801f4fc87414c69de1ab4082facd12
Jônatas <jjsfandre@gmail.com>
2019-06-26 11:30:21 -0300

    Corrigindo formatação do date.now

--- app/src/main/java/br/ufjf/dcc196/todolist/Helper.java
+++ app/src/main/java/br/ufjf/dcc196/todolist/Helper.java
@@ -1,11 +1,15 @@
 package br.ufjf.dcc196.todolist;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Helper {
 
     public static final String getDataHoraAtualFormatada(){
 
-        return new Date().toString();
+        String formato = "dd/MM/yyyy h:mm";
+        java.util.Date agora = new java.util.Date();
+        SimpleDateFormat formatter = new SimpleDateFormat(formato);
+        return formatter.format(agora);
     }
 }
