b8603e4fc4f4ee4214eadbf8aa588204c0915684
Jônatas <jjsfandre@gmail.com>
2019-06-26 17:07:13 -0300

    Adicionando Activities que serão implementadas posteriormente

---
+++ app/src/main/java/br/ufjf/dcc196/todolist/ListaTarefasByTagActivity.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc196.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaTarefasByTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_tarefas_by_tag);
+    }
+}
---
+++ app/src/main/java/br/ufjf/dcc196/todolist/NovaTagActivity.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc196.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovaTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_tag);
+    }
+}
